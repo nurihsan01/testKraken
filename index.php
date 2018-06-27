4919160d7fbe9b981f74ae4b93debd8c7641b9fe
nurihsan01 <40623414+nurihsan01@users.noreply.github.com>
2018-06-27 20:28:46 +0800

    berubahdarigithub

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 	//echo "ini achooo";
 	
 	// ini mengkoneksikan php ke database
+	$string = "kata";
 	$con = mysql_connect('localhost','root','');
 	if(!$con){
 		die('tidak bisa konek'.mysql_error());
